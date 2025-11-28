ab8724f318c1d54d98671fff12b180b8dc691c9e
Papa Abdoulaye <papaabdoulaye.ndoye.01@gmail.com>
2025-11-28 01:31:41 +0100

    feat(init): initial Spring Boot project for VisionAI

---
+++ src/main/java/org/example/visionai/VisionAiApplication.java
@@ -0,0 +1,13 @@
+package org.example.visionai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/visionai/VisionAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.visionai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
